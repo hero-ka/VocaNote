2809777992bc79bb06c40272defba41f09f16f45
hero <hero1388kurd@gmail.com>
2026-08-22 17:55:23 +0330

    chore: set up project structure

---
+++ vocanote/src/components/partials/Layout/index.tsx
@@ -0,0 +1,14 @@
+import type { ReactNode } from "react"
+
+type layoutProps = {
+    children: ReactNode;
+}
+
+const Layout = ({ children }: layoutProps) => {
+    <div className="">
+        <main>
+
+        </main>
+    </div>
+}
+export default Layout;
